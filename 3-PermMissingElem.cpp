4d939f6a13445200bbfbd01c382edb092c0597ca
Luboš Matejčík <lubos.matejcik@gmail.com>
2020-10-14 19:51:47 +0200

    Add 2. and 3. lessons

---
+++ 3-PermMissingElem.cpp
@@ -0,0 +1,43 @@
+/*
+An array A consisting of N different integers is given. The array contains integers in the range [1..(N + 1)], which means that exactly one element is missing.
+
+Your goal is to find that missing element.
+
+Write a function:
+
+int solution(vector<int> &A);
+
+that, given an array A, returns the value of the missing element.
+
+For example, given array A such that:
+
+  A[0] = 2
+  A[1] = 3
+  A[2] = 1
+  A[3] = 5
+the function should return 4, as it is the missing element.
+
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [0..100,000];
+the elements of A are all distinct;
+each element of array A is an integer within the range [1..(N + 1)].
+*/
+#include <algorithm>
+
+int solution(std::vector<int>& input) {
+  if (input.size() == 1) {
+    return input[0] == 1 ? 2 : 1;
+  }
+
+  std::sort(input.begin(), input.end());
+  input.push_back(input.size() + 2);
+
+  const std::size_t inputSize = input.size();
+  for (std::size_t i = 0; i < inputSize; i++) {
+    if (input[i] != int(i + 1))
+      return i + 1;
+  }
+
+  return -1;
+}
